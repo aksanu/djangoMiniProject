f9bc6dfd31cdbe8b53ee4eecb053f8c787793a77
aksanu <ankit21april@gmail.com>
2020-10-02 17:22:53 +0530

    added a new user model in comment

--- account/models.py
+++ account/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+"""
+class UserModel(models.Model):
+	user= models.ForeignKey(User , on_delete= models.CASCADE)
+	
+	class Meta:
+		verbose_name="UserModel"
+		ordering = ['user.name']
+
+	def __str__(self):
+		return self.user.username
+
+
+
+"""
+
 
 class UserFile(models.Model):
 	name = models.CharField(max_length = 50)
